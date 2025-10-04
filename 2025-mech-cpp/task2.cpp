86dc6dd545a2275400e27f2db13fd66db5e9d22b
st141472 <st141472@student.spbu.ru>
2025-10-04 21:44:13 +0300

    hometask upload

---
+++ 2025-mech-cpp/task2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    std::cin >> a >> b;
+    printf("%d %d\n", b - 1, a - 1);
+    return 0;
+}
